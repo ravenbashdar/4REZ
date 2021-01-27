ad0a9a8ec8f29ba13517fa143f8bb1832abd4d08
ravenbashdar <75314649+ravenbashdar@users.noreply.github.com>
2021-01-28 02:40:39 +0300

    Update Monster.py

--- Monster.py
+++ Monster.py
@@ -76,9 +76,9 @@ vuln = "\033[32mVuln"
 
 os.system("clear")
 print "\x1b[0;31m⚔═══════════════════════════☠═══════════════════════════⚔"
-print  """\x1b[0;31m [¤] \x1b[0;31mASSALAM O ALAIKUM\x1b[0;31m  \033[1;96m   [¤] \x1b[0;31m Hack Fb By eso[1;96m  
-\033[1;93m [¤] \x1b[0;31mSTAY HOME\x1b[1;96m      [¤] \x1b[0;31mFACEBOOK : TERMUX TOOLS\x1b[1;96m  
-\033[1;93m [¤] \x1b[0;31mTOOLS BY eso hawlery\x1b[1;96m  [¤] \x1b[0;31mYOUTUBE  : eso hawlery\x1b[0;31m"""
+print  """\x1b[0;31m [¤] \x1b[0;31mASSALAM O ALAIKUM\x1b[0;31m  \033[1;96m   [¤] \x1b[0;31m Hack Fb By A4REZ[1;96m  
+\033[1;93m [¤] \x1b[0;31mSTAY HOME\x1b[1;96m      [¤] \x1b[0;31mFACEBOOK : bvvv \x1b[1;96m  
+\033[1;93m [¤] \x1b[0;31mTOOLS BY Arez\x1b[1;96m  [¤] \x1b[0;31mYOUTUBE  : Arez\x1b[0;31m"""
 print " \x1b[1;93m⚔══════════════════════════☠═══════════════════════════⚔"
 
 CorrectUsername = "Our"
